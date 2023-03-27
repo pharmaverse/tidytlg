439ef34132a74d0a83ac25270452414282c2c65e
elimillera <elimillera@gmail.com>
2023-03-27 20:19:39 +0000

    Don't run file path tests on non-linux

--- tests/testthat/test-getFileName.R
+++ tests/testthat/test-getFileName.R
@@ -4,6 +4,8 @@ test_that("getFileNameBatch returns the expected results", {
   cmdArgs2 <- c("--arg1", "someThing", "-e", 'timber::axecute(file="/asdf/programs/CopyOftest_demo.R",log_name="CopyOftest_demo.R.log",log_path="/adr/PDEV/emiller9/pharma/test_compound/mi_test/dbr_tidytlg/re_test/output")')
   cmdArgs3 <- c("--arg1", "someThing", "--file=/asdf/programs/CopyOftest_demo.R")
 
+  skip_on_os(c("windows", "mac", "solaris"))
+
   expect_equal(
     suppressWarnings(getFileNameBatch(cmdArgs1)),
     "/asdf/programs/CopyOftest_demo.R"
