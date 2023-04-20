3df13244694d4fd27f6f96330ca89ef08bd9758c
elimillera <elimillera@gmail.com>
2023-04-20 16:32:08 +0000

    Update test file path

--- tests/testthat/test-getFileName.R
+++ tests/testthat/test-getFileName.R
@@ -1,7 +1,7 @@
 test_that("getFileNameBatch returns the expected results", {
 
-  cmdArgs1 <- c("--arg1", "someThing", "-e", 'timber::axecute(file~+~=~+~"/asdf/programs/CopyOftest_demo.R",log_name="CopyOftest_demo.R.log",log_path="/adr/PDEV/emiller9/pharma/test_compound/mi_test/dbr_tidytlg/re_test/output")')
-  cmdArgs2 <- c("--arg1", "someThing", "-e", 'timber::axecute(file="/asdf/programs/CopyOftest_demo.R",log_name="CopyOftest_demo.R.log",log_path="/adr/PDEV/emiller9/pharma/test_compound/mi_test/dbr_tidytlg/re_test/output")')
+  cmdArgs1 <- c("--arg1", "someThing", "-e", 'timber::axecute(file~+~=~+~"/asdf/programs/CopyOftest_demo.R",log_name="CopyOftest_demo.R.log",log_path="/wrkdir/output")')
+  cmdArgs2 <- c("--arg1", "someThing", "-e", 'timber::axecute(file="/asdf/programs/CopyOftest_demo.R",log_name="CopyOftest_demo.R.log",log_path="/wrkdir/output")')
   cmdArgs3 <- c("--arg1", "someThing", "--file=/asdf/programs/CopyOftest_demo.R")
 
   skip_on_os(c("windows", "mac", "solaris"))
