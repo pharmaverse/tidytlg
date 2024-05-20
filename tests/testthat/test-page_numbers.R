5826c347bf53f93fabebe4423001384e9ccc8889
Konrad Pagacz <konrad.pagacz@gmail.com>
2024-05-20 09:47:04 +0200

    fix: fixed pagenum = TRUE breaking rendering multiple tables to one rtf file

---
+++ tests/testthat/test-page_numbers.R
@@ -0,0 +1,36 @@
+library(tibble)
+
+test_that("Page numbers are added on each page", {
+  withr::with_options(
+    list(tidytlg.add_datetime = FALSE),
+    {
+      mtcars2 <- mtcars %>%
+        rownames_to_column(var = "USUBJID") %>%
+        mutate(
+          am = factor(am),
+          cyl = factor(cyl),
+          gear = factor(gear)
+        )
+
+      tab <- freq(
+        mtcars2,
+        colvar = "cyl",
+        rowvar = "am"
+      ) %>%
+        select(-c("row_type", "group_level"))
+      tab2 <- tab
+
+      withr::with_dir(
+        new = test_path("test_outputs"),
+        code = {
+          gentlg(
+            list(tab, tab2),
+            file = "pagenumbers",
+            pagenum = TRUE
+          )
+        }
+      )
+    }
+  )
+  expect_snapshot_file(test_path("test_outputs/pagenumbers.rtf"))
+})
