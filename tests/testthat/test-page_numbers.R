a0e68f9f3f8687d63db4f8ba8279c77184588ba6
Konrad Pagacz <konrad.pagacz@gmail.com>
2024-05-28 16:03:57 +0200

    * changed the display of the tlf type for tables to Table instead of the default Listing

--- tests/testthat/test-page_numbers.R
+++ tests/testthat/test-page_numbers.R
@@ -26,6 +26,7 @@ test_that("Page numbers are added on each page", {
           gentlg(
             list(tab, tab2),
             file = "pagenumbers",
+            tlf = "Listing",
             pagenum = TRUE
           )
         }
@@ -33,4 +34,39 @@ test_that("Page numbers are added on each page", {
     }
   )
   expect_snapshot_file(test_path("test_outputs/pagenumbers.rtf"))
+})
+
+test_that("The page number mentions the table", {
+  withr::with_options(
+    list(tidytlg.add_datetime = FALSE),
+    {
+      mtcars2 <- mtcars %>%
+        rownames_to_column(var = "USUBJID") %>%
+        mutate(
+          am = factor(am),
+          cyl = factor(cyl),
+          gear = factor(gear)
+        )
+
+      tab <- freq(
+        mtcars2,
+        colvar = "cyl",
+        rowvar = "am"
+      ) %>%
+        select(-c("row_type", "group_level"))
+      tab2 <- tab
+
+      withr::with_dir(
+        new = test_path("test_outputs"),
+        code = {
+          gentlg(
+            list(tab, tab2),
+            file = "pagenumbersTable",
+            pagenum = TRUE
+          )
+        }
+      )
+    }
+  )
+  expect_snapshot_file(test_path("test_outputs/pagenumbersTable.rtf"))
 })
