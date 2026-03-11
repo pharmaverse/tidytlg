4429c7d740315c4ca2457494f8b7f93b327123ac
Ezequiel Anokian <lainjoso@hotmail.com>
2026-03-11 11:35:16 +0100

    Update gentlg_single.R

--- R/gentlg_single.R
+++ R/gentlg_single.R
@@ -483,8 +483,10 @@ gentlg_single <- function(huxme = NULL,
     # 2 whitespaces represent an indentation of 0.125 inches = 180 twips
     # e.g. 4 whitespaces represent a left-indentation of 0.25 inches = 360 twips
     num_twips <- num_whitespaces * 90
-    raw_rtf_markup <- paste0("\\intbl\\li", num_twips, "\\fi0")
-    x <- paste0(raw_rtf_markup, " ", trimws(x, "left"))
+    if (num_twips > 0) {
+      raw_rtf_markup <- paste0("\\intbl\\li", num_twips, "\\fi0")
+      x <- paste0(raw_rtf_markup, " ", trimws(x, "left"))
+    }
     return(x)
   }
 
